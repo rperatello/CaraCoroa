bf83dc4780ca233b97350a8c085dba6f02a14f97
Roger <roger.peratello@hotmail.com>
2022-11-10 23:57:28 -0300

    changes

---
+++ Shared/CaraCoroaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaraCoroaApp.swift
+//  Shared
+//
+//  Created by Roger Peratello on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CaraCoroaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
